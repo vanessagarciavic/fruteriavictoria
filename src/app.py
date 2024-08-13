8313aa3e2be71baff98a248b8e86be690c46f1d8
vanessagarciavic <vanessavictoria2001garcia@gmail.com>
2024-08-12 20:02:29 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -29,7 +29,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'vanepotter19@gmail.com'
-app.config['MAIL_PASSWORD'] = 'dbjq aojm vnfn vhpf'  
+app.config['MAIL_PASSWORD'] = ''  
 
 mail = Mail(app)
 
